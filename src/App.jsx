8182e68c4498712abceb48702ad418a8bcb39efb
M-uskan <aryamuskan115@gmail.com>
2024-06-03 00:19:30 +0530

    firstcommit

---
+++ src/App.jsx
@@ -0,0 +1,112 @@
+import { useState, useCallback, useEffect, useRef } from 'react'
+
+
+
+function App() {
+  const [length, setLength] = useState(8)
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false)
+  const [password, setPassword] = useState("")
+
+  //useRef hook
+  const passwordRef = useRef(null)
+
+  const passwordGenerator = useCallback(() => {
+    let pass = ""
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    if (numberAllowed) str += "0123456789"
+    if (charAllowed) str += "!@#$%^&*-_+=[]{}~`"
+
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1)
+      pass += str.charAt(char)
+      
+    }
+
+    setPassword(pass)
+
+
+  }, [length, numberAllowed, charAllowed, setPassword])
+
+  const copyPasswordToClipboard = useCallback(() => {
+    passwordRef.current?.select();
+    passwordRef.current?.setSelectionRange(0, 999);
+    window.navigator.clipboard.writeText(password)
+  }, [password])
+
+  useEffect(() => {
+    passwordGenerator()
+  }, [length, numberAllowed, charAllowed, passwordGenerator])
+  return (
+    
+    <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500">
+      <h1 className='text-white text-center my-3'>Password generator</h1>
+    <div className="flex shadow rounded-lg overflow-hidden mb-4">
+        <input
+            type="text"
+            value={password}
+            className="outline-none w-full py-1 px-3"
+            placeholder="Password"
+            readOnly
+            ref={passwordRef}
+        />
+        <button
+        onClick={copyPasswordToClipboard}
+        className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'
+        >copy</button>
+        
+    </div>
+    <div className='flex text-sm gap-x-2'>
+      <div className='flex items-center gap-x-1'>
+        <input 
+        type="range"
+        min={6}
+        max={100}
+        value={length}
+         className='cursor-pointer'
+         onChange={(e) => {setLength(e.target.value)}}
+          />
+          <label>Length: {length}</label>
+      </div>
+      <div className="flex items-center gap-x-1">
+      <input
+          type="checkbox"
+          defaultChecked={numberAllowed}
+          id="numberInput"
+          onChange={() => {
+              setNumberAllowed((prev) => !prev);
+          }}
+      />
+      <label htmlFor="numberInput">Numbers</label>
+      </div>
+      <div className="flex items-center gap-x-1">
+          <input
+              type="checkbox"
+              defaultChecked={charAllowed}
+              id="characterInput"
+              onChange={() => {
+                  setCharAllowed((prev) => !prev )
+              }}
+          />
+          <label htmlFor="characterInput">Characters</label>
+      </div>
+    </div>
+</div>
+    
+  )
+}
+
+export default App
+//useref is bascially used for refernce 
+//setpassowrd is used for selection of password when u copy it for better user experience
+//window.navigator.clipboard.writeText(password)
+//}, [password]) is basicaaly used to co[y the password to clipboard.
+//callback hook is used for optimization when any dependency is changed the setpassword should be changed but also optimized.
+// all are stored in cache for re rendering
+//the useeffect is used when anything of the dependency changes we need to reload the and change the passowrd.
+//    passwordRef.current?.setSelectionRange(0, 999); is used to only select the selected range
+//agar hum callback main dependency main assword denge setpassword ke jagah to voh harbaar change krta rehga ,infinite loop man chale jayenge
+//useeffect jab bbhi page load hota h ,toh sabse phle yeh call hunda h,agagr dependncy main kuch change hua toh fer se rerun hojayega
+//passowrd ref s used for reference so that mainuplations can be made we pass passswordrefin{ref}
+
+
